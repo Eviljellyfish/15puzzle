3db308dd36a5da1d94f8cc885b2ad29bf203b29f
Eviljellyfish <Eviljellyfish@rambler.ru>
2021-04-30 19:49:17 +0300

    Added Tile script

---
+++ 15-puzzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Tiles;
+    private Camera _camera;
+    private int[,] board;
+    void Start()
+    {
+        board = new int[4,4];
+        Debug.Log(board.Rank);
+        _camera = Camera.main;
+        initiateBoard(board);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void initiateBoard(int[,] board) {
+        for (int i=0; i<board.Length; i++) {
+            for (int j=0; j<board.Length; j++) {
+                int index = i+j*board.Length;
+                Tiles[i+j].GetComponent<Tile>().initialize(index+1, i, j);
+            }
+        }
+    }
+}
---
+++ 15-puzzle/Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Tile : MonoBehaviour, IPointerClickHandler
+{
+    private int index, posX, posY;
+    public void initialize(int index, int posX, int posY) {
+        this.index = index;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log(this.name+index+posX+posY);
+    }
+}
